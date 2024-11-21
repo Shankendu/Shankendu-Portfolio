45c41660e351867d19d7e6573ac806da711756e4
ShankenduK <shankendu@gmail.com>
2024-11-22 03:07:34 +0530

    Added About and Work Section in Homepage -v1

--- src/components/HamburgerMenu.jsx
+++ src/components/HamburgerMenu.jsx
@@ -1,21 +1,147 @@
 /* eslint-disable react/prop-types */
-
-
-
-
+import githubDark from "../assets/github-dark.png";
+import githubLight from "../assets/github-light.png";
+import linkedinDark from "../assets/linkedin-dark.png";
+import linkedinLight from "../assets/linkedin-light.png";
+import instaDark from "../assets/instagram-dark.png";
+import instaLight from "../assets/instagram-light.png";
+import facebookDark from "../assets/facebook-dark.png";
+import facebookLight from "../assets/facebook-light.png";
+import youtubeDark from "../assets/youtube-dark.png";
+import youtubeLight from "../assets/youtube-light.png";
 
 const HamburgerMenu = (props) => {
-
- 
   return (
     <>
-     <div  id="ham" className={`bg-black w-full h-screen ${props.className} transition-all duration-1000 ease-in-out`}>
-    <div onClick={props.onClick} className="text-white text-2xl">
-      X
-    </div>
-     </div> 
-    </>
-  )
-}
+      <div
+        id="ham"
+        className={`dark:bg-[#2C2C2C] bg-[#333333] w-full h-screen ${props.className} transition-all duration-1000 ease-in-out px-6 py-7`}
+      >
+        {/*Animated Close Button Svg*/}
+        <div
+          onClick={props.onClick}
+          className="dark:text-[#EAEAEA] text-[#F0F0F0] text-2xl w-full flex items-center justify-end "
+        >
+          <div
+            className={`cursor-pointer shadow-[0_10px_30px_rgba(8,_112,_184,_0.7)] dark:bg-[#5B84FF] bg-[#5B84FF] dark:hover:bg-[#4A73D1] hover:bg-[#4A73D1] w-12 h-12 flex items-center justify-center rounded-full transition-transform duration-1000 ease-in-out delay-300  ${props.closeClassname}`}
+          >
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              strokeWidth={1.5}
+              stroke="currentColor"
+              className={`w-6 h-6`}
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                d="M6 18L18 6M6 6l12 12"
+              />
+            </svg>
+          </div>
+        </div>
 
-export default HamburgerMenu
+        {/* Navigation Menu */}
+        <div className="h-[80vh] w-full flex flex-col justify-between items-start">
+          <div className={`mt-10 w-full transition-transform duration-1000 ease-in-out delay-300 ${props.menuClassname}`}>
+            <h1 className="font-poppins text-base dark:text-[#A3A3A3] text-[#6B7280] pb-3 mb-5 border-b-2 dark:border-[#A3A3A3] border-[#6B7280]">
+              NAVIGATION
+            </h1>
+            <ul className=" flex flex-col w-full items-start text-4xl gap-5 dark:text-[#EAEAEA] text-[#F0F0F0]">
+              <li
+                to={"/"}
+                className="font-poppins cursor-pointer relative w-full hover:after:transition-all hover:after:duration-200 hover:after:delay-75 hover:after:ease-in hover:after:content-['_'] [&.active]:after:content-['_'] hover:after:w-3 [&.active]:after:w-3 after:h-0 after:w-0 hover:after:h-3 [&.active]:after:h-3 hover:after:dark:bg-[#EAEAEA] [&.active]:after:dark:bg-[#EAEAEA] [&.active]:after:bg-[#333333] hover:after:bg-[#333333] hover:after:rounded-full [&.active]:after:rounded-full hover:after:absolute [&.active]:after:absolute hover:after:top-[35%] [&.active]:after:top-[35%] hover:after:right-[5%] [&.active]:after:right-[5%] hover:after:z-10 [&.active]:after:z-10"
+              >
+                Home
+              </li>
+              <li
+                to={"/about"}
+                className="font-poppins cursor-pointer relative w-full hover:after:transition-all hover:after:duration-200 hover:after:delay-75 hover:after:ease-in hover:after:content-['_'] [&.active]:after:content-['_'] hover:after:w-3 [&.active]:after:w-3 after:h-0 after:w-0 hover:after:h-3 [&.active]:after:h-3 hover:after:dark:bg-[#EAEAEA] [&.active]:after:dark:bg-[#EAEAEA] [&.active]:after:bg-[#333333] hover:after:bg-[#333333] hover:after:rounded-full [&.active]:after:rounded-full hover:after:absolute [&.active]:after:absolute hover:after:top-[35%] [&.active]:after:top-[35%] hover:after:right-[5%] [&.active]:after:right-[5%] hover:after:z-10 [&.active]:after:z-10"
+              >
+                About
+              </li>
+              <li className="font-poppins cursor-pointer relative w-full hover:after:transition-all hover:after:duration-200 hover:after:delay-75 hover:after:ease-in hover:after:content-['_'] [&.active]:after:content-['_'] hover:after:w-3 [&.active]:after:w-3 after:h-0 after:w-0 hover:after:h-3 [&.active]:after:h-3 hover:after:dark:bg-[#EAEAEA] [&.active]:after:dark:bg-[#EAEAEA] [&.active]:after:bg-[#333333] hover:after:bg-[#333333] hover:after:rounded-full [&.active]:after:rounded-full hover:after:absolute [&.active]:after:absolute hover:after:top-[35%] [&.active]:after:top-[35%] hover:after:right-[5%] [&.active]:after:right-[5%] hover:after:z-10 [&.active]:after:z-10">
+                Projects
+              </li>
+              <li className="font-poppins cursor-pointer relative w-full hover:after:transition-all hover:after:duration-200 hover:after:delay-75 hover:after:ease-in hover:after:content-['_'] [&.active]:after:content-['_'] hover:after:w-3 [&.active]:after:w-3 after:h-0 after:w-0 hover:after:h-3 [&.active]:after:h-3 hover:after:dark:bg-[#EAEAEA] [&.active]:after:dark:bg-[#EAEAEA] [&.active]:after:bg-[#333333] hover:after:bg-[#333333] hover:after:rounded-full [&.active]:after:rounded-full hover:after:absolute [&.active]:after:absolute hover:after:top-[35%] [&.active]:after:top-[35%] hover:after:right-[5%] [&.active]:after:right-[5%] hover:after:z-10 [&.active]:after:z-10">
+                Contact
+              </li>
+            </ul>
+          </div>
+
+          {/* Social Links */}
+          <div className={`w-full pb-4 transition-transform duration-1000 ease-in-out delay-[400ms] ${props.socialClassname}`}>
+            <h1 className="font-poppins text-base dark:text-[#A3A3A3] text-[#6B7280] pb-3 mb-5 border-b-2 dark:border-[#A3A3A3] border-[#6B7280]">
+              SOCIALS
+            </h1>
+            <ul className="flex items-center justify-start gap-5">
+              <li>
+                <img
+                  src={githubDark}
+                  alt="github"
+                  className="hidden dark:block h-9 cursor-pointer"
+                />
+                <img
+                  src={githubLight}
+                  alt="github"
+                  className="block dark:hidden h-9 cursor-pointer"
+                />
+              </li>
+              <li>
+                <img
+                  src={linkedinDark}
+                  alt="linkedin"
+                  className="hidden dark:block h-9 cursor-pointer"
+                />
+                <img
+                  src={linkedinLight}
+                  alt="linkedin"
+                  className="block dark:hidden h-9 cursor-pointer"
+                />
+              </li>
+              <li>
+                <img
+                  src={instaDark}
+                  alt="instagram"
+                  className="hidden dark:block h-9 cursor-pointer"
+                />
+                <img
+                  src={instaLight}
+                  alt="instagram"
+                  className="block dark:hidden h-9 cursor-pointer"
+                />
+              </li>
+              <li>
+                <img
+                  src={facebookDark}
+                  alt="facebook"
+                  className="hidden dark:block h-9 cursor-pointer"
+                />
+                <img
+                  src={facebookLight}
+                  alt="facebook"
+                  className="block dark:hidden h-9 cursor-pointer"
+                />
+              </li>
+              <li>
+                <img
+                  src={youtubeDark}
+                  alt="youtube"
+                  className="hidden dark:block h-9 cursor-pointer"
+                />
+                <img
+                  src={youtubeLight}
+                  alt="youtube"
+                  className="block dark:hidden h-9 cursor-pointer"
+                />
+              </li>
+            </ul>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default HamburgerMenu;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,4 +1,5 @@
 import male2 from "../assets/characterMale2.png";
+import work from "../assets/work.png";
 import favAudio from "../assets/favAudio.mp3";
 import Navbar from "../components/Navbar";
 import Preload from "../components/Preload";
@@ -19,7 +20,7 @@ const Home = () => {
     setLoading(true);
     audio.addEventListener("ended", () => setIsPlaying(false));
     return () => {
-      audio.removeEventListener('ended', () => setIsPlaying(false));
+      audio.removeEventListener("ended", () => setIsPlaying(false));
     };
   }, []);
   useEffect(() => {
@@ -78,11 +79,21 @@ const Home = () => {
   return (
     <>
       <div
-        className="w-full min-h-screen relative overflow-hidden dark:bg-[#1A1A1A] bg-[#F0F0F0]"
+        className="w-full min-h-screen relative overflow-hidden dark:bg-[#1A1A1A] bg-[#F0F0F0] "
         ref={container}
       >
         <Preload id="preload" className="absolute z-20" />
-        <HamburgerMenu onClick={() => setOpen(!open)} className={`block sm:hidden absolute z-30 top-0 right-0 ${open ? "translate-x-0" : "translate-x-full"}`}/>
+        <HamburgerMenu
+          onClick={() => setOpen(!open)}
+          className={`block sm:hidden absolute z-30 top-0 right-0 ${
+            open ? "translate-x-0" : "translate-x-full"
+          }`}
+          closeClassname={` ${
+            open ? "scale-[1] translate-x-0" : "scale-0 translate-x-full"
+          }`}
+          menuClassname={` ${open ? "translate-x-0" : "translate-x-full"}`}
+          socialClassname={` ${open ? "translate-x-0" : "translate-x-full"}`}
+        />
         <Navbar onClick={() => setOpen(!open)} id="navbar" />
         <div
           id="home"
@@ -135,27 +146,7 @@ const Home = () => {
           <div className="w-full md:w-[380px] lg:w-[500px] h-[200px] flex md:flex-col items-center justify-between md:items-end md:justify-evenly">
             {/* Audio Component */}
             <div
-              className="w-10 h-10 border-2 relative dark:border-[#4A73D1] rounded-full ml-5 md:mr-10 hidden dark:flex items-center justify-center cursor-pointer before:w-10 before:h-10 before:border-2 before:border-[#4A73D1] before:absolute before:top-[-5%] before:left-[-5%] before:translate-x-[-50%] before:translate-y-[-50%] before:rounded-full before:animate-ripple"
-              onClick={playPause}
-            >
-              {isPlaying ? (
-                <lord-icon
-                  src="https://cdn.lordicon.com/jctchmfs.json"
-                  trigger="hover"
-                  colors="primary:#4A73D1"
-                  style={{ width: "20px", height: "20px" }}
-                ></lord-icon>
-              ) : (
-                <lord-icon
-                  src="https://cdn.lordicon.com/becebamh.json"
-                  trigger="hover"
-                  colors="primary:#4A73D1"
-                  style={{ width: "20px", height: "20px" }}
-                ></lord-icon>
-              )}
-            </div>
-            <div
-              className="w-10 h-10 border-2 relative border-[#5B84FF] rounded-full ml-5 md:mr-10 dark:hidden flex items-center justify-center cursor-pointer before:w-10 before:h-10 before:border-2 before:border-[#5B84FF] before:absolute before:top-[-5%] before:left-[-5%] before:translate-x-[-50%] before:translate-y-[-50%] before:rounded-full before:animate-ripple"
+              className="w-10 h-10 border-2 relative dark:border-[#5B84FF] border-[#5B84FF] rounded-full ml-5 md:mr-10 flex items-center justify-center cursor-pointer before:w-10 before:h-10 before:border-2 before:dark:border-[#5B84FF] before:border-[#5B84FF] before:absolute before:top-[-5%] before:left-[-5%] before:translate-x-[-50%] before:translate-y-[-50%] before:rounded-full before:animate-ripple"
               onClick={playPause}
             >
               {isPlaying ? (
@@ -186,22 +177,87 @@ const Home = () => {
           {/* Name Marquee */}
           <div
             id="marquee"
-            className="flex items-center h-[50px] w-[200%] text-[max(2em,_16vw)] md:text-[max(6em,_16vw)] dark:text-[#EAEAEA] text-[#333333] relative selection:bg-transparent dark:selection:bg-transparent"
+            className="flex items-center h-[50px] w-[200%] text-[max(2em,_16vw)] md:text-[max(6em,_16vw)] dark:text-[#EAEAEA] text-[#333333] relative selection:bg-transparent dark:selection:bg-transparent group/marquee cursor-pointer"
             draggable="false"
           >
             <div
-              className="absolute w-full left-[100%] animate-scroll-left translate-y-[-150%] sm:translate-y-[-60%] md:translate-y-[-50%] lg:translate-y-[-45%] will-change-transform"
+              className="absolute w-full left-[100%] animate-scroll-left translate-y-[-150%] sm:translate-y-[-60%] md:translate-y-[-50%] lg:translate-y-[-45%] will-change-transform group-hover/marquee:[animation-play-state:paused]"
               style={{ "--position": 1 }}
             >
               <h1 className="whitespace-nowrap ">Shankendu Kunti -</h1>
             </div>
             <div
-              className="absolute w-full left-[100%] animate-scroll-left translate-y-[-150%] sm:translate-y-[-60%] md:translate-y-[-50%] lg:translate-y-[-45%] will-change-transform"
+              className="absolute w-full left-[100%] animate-scroll-left translate-y-[-150%] sm:translate-y-[-60%] md:translate-y-[-50%] lg:translate-y-[-45%] will-change-transform group-hover/marquee:[animation-play-state:paused]"
               style={{ "--position": 2 }}
             >
               <h1 className="whitespace-nowrap">Shankendu Kunti -</h1>
             </div>
           </div>
+        </div>
+      </div>
+
+      <div className="dark:bg-[#eaeaea] bg-[#333333] min-h-screen w-full font-poppins dark:text-[#1a1a1a] text-[#F0F0F0] p-20">
+        {/* About Section */}
+        <div className=" w-full h-fit flex items-start justify-center px-5 pt-5 border-b-2 dark:border-[#A3A3A3] border-[#F0F0F0] ">
+          <div className=" px-5 pt-5 h-[inherit] flex flex-col items-start justify-start gap-y-24">
+            <h1 className="text-3xl leading-relaxed  ">
+              I’m a passionate web developer with expertise in creating
+              seamless, responsive, and impactful websites. Driven by curiosity
+              and innovation, I transform ideas into digital experiences that
+              leave a lasting impression.
+            </h1>
+            <h1 className="text-base">RECENT PROJECT</h1>
+          </div>
+          <div className=" flex flex-col gap-y-10 justify-start items-start p-8">
+            <h1>
+              I focus on creating user-centered designs with clean code,
+              delivering tailored solutions that make every project stand out.
+            </h1>
+            <button className="group relative z-0 h-44 w-44 shadow-xl group-hover:shadow-2xl flex items-center justify-center overflow-hidden overflow-x-hidden rounded-full dark:bg-[#1a1a1a] bg-[#F0F0F0]  dark:text-[#eaeaea] text-[#333333]">
+              <span className="relative z-10">About Me</span>
+              <span className="absolute inset-0 overflow-hidden rounded-md">
+                <span className="absolute left-0 aspect-square w-full origin-center translate-x-full rounded-full bg-[#5B84FF] transition-all duration-500 group-hover:-translate-x-0 group-hover:scale-150"></span>
+              </span>
+            </button>
+          </div>
+        </div>
+
+        {/* Work Section */}
+        <div className="w-full h-fit p-10 border-b-2 dark:border-[#A3A3A3] border-[#F0F0F0] flex items-center justify-between">
+          <h1 className=" text-7xl">CRYPTOTRACKR</h1>
+
+          <div className="h-[10em] w-[17em] rounded-[1em] overflow-hidden relative group p-4 z-0">
+            <div className="circle absolute h-[5em] w-[5em] -top-[2.5em] -right-[2.5em] rounded-full bg-[#5B84FF] group-hover:scale-[800%] duration-500 group-hover:opacity-60 group-hover:blur-md"></div>
+
+            <button className="text-[0.8em] absolute bottom-[1em] left-[1em] group-hover:dark:text-[#eaeaea] group-hover:text-[#333333] duration-500">
+              <span className="relative before:h-[0.16em] before:absolute before:w-full before:content-[''] group-hover:before:bg-[white] duration-100 before:bottom-0 before:left-0 scale-0 group-hover:scale-100">
+                source code
+              </span>
+              <i className="fa-solid fa-arrow-right"></i>
+            </button>
+
+            <button className="absolute font-poppins scale-0 group-hover:scale-100 group-hover:dark:text-[#eaeaea] group-hover:text-[#333333]  duration-500 text-[1.4em]">
+              VIEW PAGE
+            </button>
+            <img
+              src={work}
+              alt="work"
+              className="scale-[1.7] absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 -z-30 group-hover:blur-[2px] group-hover:scale-[1.5] duration-300 "
+            />
+          </div>
+        </div>
+
+        {/* View More Button */}
+        <div className="w-full h-fit px-20 pt-20 flex items-center justify-center">
+          <button className="group relative inline-flex h-24 w-60 items-center justify-center overflow-hidden rounded-full border dark:border-[#A3A3A3] border-[#F0F0F0]  bg-transparent font-medium">
+            <div className="inline-flex h-12 translate-y-0 items-center justify-center px-6 text-neutral-950 transition duration-[600ms] group-hover:-translate-y-[150%]">
+              More Projects
+            </div>
+            <div className="absolute inline-flex h-36 w-full translate-y-[100%] items-center justify-center text-neutral-50 transition duration-500 group-hover:translate-y-0">
+              <span className="absolute h-full w-full translate-y-full skew-y-12 scale-y-0 bg-blue-500 transition duration-[600ms] group-hover:translate-y-0 group-hover:scale-[1.7]"></span>
+              <span className="z-10">More Projects</span>
+            </div>
+          </button>
         </div>
       </div>
     </>
